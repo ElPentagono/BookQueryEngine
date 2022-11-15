1a0e12a4c78c79b9180ec18dd1d2f384a366e429
jorge <jorge.hernandez.hdez.02@gmail.com>
2022-11-15 11:01:01 +0000

    Tokenizer finished

--- indexer/src/main/java/es/pentagono/tokenizers/GutenbergTokenizer.java
+++ indexer/src/main/java/es/pentagono/tokenizers/GutenbergTokenizer.java
@@ -1,34 +1,61 @@
 package es.pentagono.tokenizers;
 
+import es.pentagono.Document;
 import es.pentagono.Tokenizer;
 
 import java.net.URL;
 import java.nio.file.Files;
 import java.nio.file.Path;
-import java.util.Collections;
-import java.util.List;
-import java.util.StringTokenizer;
+import java.util.*;
 import java.util.stream.Collectors;
 
 public class GutenbergTokenizer implements Tokenizer {
 
-
     private static final List<String> stopwords = loadStopwords();
+    private final Map<String, List<Integer>> tokenizeContent = new HashMap<>();
 
     @Override
-    public List<String> tokenize(String content) {
-        String s = content.replaceAll("[\\p{Punct}[0-9]+_\t\n\\x0B\f\n\r-]", " ");
-        return Collections.list(new StringTokenizer(s, " ")).stream()
+    public Map<String, List<Integer>> tokenize(Document document) {
+        String s = document.content.replaceAll("[\\p{Punct}[0-9]+_\t\n\\x0B\f\n\r-]", " ");
+        return processDocument(Collections.list(new StringTokenizer(s, " ")).stream()
                 .map(token -> (String) token)
-                .collect(Collectors.toList());
+                .collect(Collectors.toList()));
     }
     public static List<String> loadStopwords() {
         try {
             URL stopwords = GutenbergTokenizer.class.getClassLoader().getResource("stopwords.txt");
-            return Files.readAllLines(Path.of(stopwords.toURI())); // TODO
+            return Files.readAllLines(Path.of(stopwords.toURI()));
         } catch (Exception e) {
             throw new RuntimeException(e);
         }
+    }
+
+    private Map<String, List<Integer>> processDocument(List<String> content) {
+        for (int i = 0; i < content.size(); i++) {
+            if (content.get(i).equals("")) continue;
+            processWord(content, i);
+        }
+        return tokenizeContent;
+    }
+
+    private void processWord(List<String> content, int i) {
+        if (check(content.get(i).toLowerCase())) return;
+        addOccurrence(content.get(i).toLowerCase(), i);
+    }
+
+    private void addOccurrence(String word, int occurrence) {
+        List<Integer> occurrences = tokenizeContent.get(word);
+        if (occurrences == null) {
+            createListOfOccurrences(word, occurrence);
+            return;
+        }
+        occurrences.add(occurrence);
+    }
+
+    private void createListOfOccurrences(String word, int occurrence) {
+        ArrayList<Integer> list = new ArrayList<>();
+        list.add(occurrence);
+        tokenizeContent.put(word, list);
     }
 
     public boolean check(String word) {
