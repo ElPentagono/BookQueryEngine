8f1a97299947ffc4c61d6fdbdbce03835249f342
JuanCarss <90870199+JuanCarss@users.noreply.github.com>
2022-11-22 22:35:21 +0000

    Inverted Index new functionalities

--- indexer/src/main/java/es/pentagono/Main.java
+++ indexer/src/main/java/es/pentagono/Main.java
@@ -8,43 +8,19 @@ import es.pentagono.serializers.TsvInvertedIndexSerializer;
 import es.pentagono.stores.FileSystemInvertedIndexStore;
 import es.pentagono.tokenizers.GutenbergTokenizer;
 
-import java.io.File;
-import java.io.IOException;
-import java.nio.file.Files;
-import java.nio.file.Path;
-import java.nio.file.Paths;
-import java.nio.file.StandardCopyOption;
-
 public class Main {
     public static void main(String[] args) {
-        DatalakeWatcher watcher = new DatalakeWatcher(
+        DocumentProcessor processor = new DocumentProcessor(
                 new FileSystemDocumentLoader(),
                 new InvertedIndexBuilder(new GutenbergTokenizer()),
                 new FileSystemInvertedIndexStore(
                         new TsvInvertedIndexSerializer(),
                         new TsvEventSerializer(),
                         new FileSystemInvertedIndexPersister()
-                ));
-        moveToTemp();
+        ));
+        new Updater(processor).update();
+        DatalakeWatcher watcher = new DatalakeWatcher();
+        watcher.addObserver(processor);
         watcher.watch();
-    }
-
-    private static void moveToTemp() {
-        try {
-            Path tmpdir = Files.createTempDirectory(System.getenv("java.io.tmpdir"));
-            moveFiles(
-                    new File(String.valueOf(Paths.get(System.getenv("DATALAKE") + "/documents"))).toPath(),
-                    tmpdir);
-            moveFiles(
-                    tmpdir,
-                    new File(String.valueOf(Paths.get(System.getenv("DATALAKE") + "/documents"))).toPath());
-            tmpdir.toFile().deleteOnExit();
-        } catch (Exception e) {
-            throw new RuntimeException(e);
-        }
-    }
-
-    private static void moveFiles(Path origin, Path target) throws IOException {
-        Files.move(origin, target, StandardCopyOption.REPLACE_EXISTING);
     }
 }
