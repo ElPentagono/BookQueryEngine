44811c5cea4482fbf4cfd5d4afd5fc7205dabd20
Jose Juan <josejuanhernandezgalvez@gmail.com>
2022-11-26 17:28:17 +0000

    Project Base created

---
+++ webservice/src/main/java/es/pentagono/commands/DocumentWordsCommand.java
@@ -0,0 +1,12 @@
+package es.pentagono.commands;
+
+import es.pentagono.Command;
+
+import java.util.Map;
+
+public class DocumentWordsCommand implements Command {
+    @Override
+    public String execute(Map<String, String> parameters) {
+        return null;
+    }
+}
---
+++ webservice/src/main/java/es/pentagono/commands/StatsTypeCommand.java
@@ -0,0 +1,12 @@
+package es.pentagono.commands;
+
+import es.pentagono.Command;
+
+import java.util.Map;
+
+public class StatsTypeCommand implements Command {
+    @Override
+    public String execute(Map<String, String> parameters) {
+        return null;
+    }
+}
