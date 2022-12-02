80ba83fc589a4545fca27db9e209c9ca94434f2c
Davoestacogido <davidcruzsanchez111@gmail.com>
2022-12-02 23:38:59 +0000

    start filters word command

--- webservice/src/main/java/es/pentagono/commands/DocumentWordsCommand.java
+++ webservice/src/main/java/es/pentagono/commands/DocumentWordsCommand.java
@@ -1,32 +1,72 @@
 package es.pentagono.commands;
 
 import es.pentagono.Command;
+import es.pentagono.Metadata;
+import es.pentagono.MetadataDeserializer;
+import es.pentagono.deserializers.GsonMetadataDeserializer;
 
 import java.io.IOException;
 import java.nio.file.Files;
 import java.nio.file.Path;
-import java.util.Arrays;
-import java.util.List;
-import java.util.Map;
+import java.util.*;
+import java.util.regex.Pattern;
 import java.util.stream.Collectors;
 
 public class DocumentWordsCommand implements Command {
 
+    private static final Map<String, Metadata> metadatas = new HashMap<>();
+    private static final MetadataDeserializer deserializer = new GsonMetadataDeserializer();
+
     @Override
     public String execute(Map<String, String> parameters) {
-        String[] split = parameters.get(":words").split("\\+");
-
-        return Arrays.stream(split)
+        return Arrays.stream(parameters.get(":words").split("\\+"))
                 .map(DocumentWordsCommand::toPath)
                 .map(DocumentWordsCommand::getLines)
+                .map(lines -> filterTo(lines, parameters.getOrDefault("to", null)))
+                .map(lines -> filterFrom(lines, parameters))
+                .map(lines -> filterAuthor(lines, parameters))
                 .collect(Collectors.toList()).toString();
+    }
+
+    private List<String> filterAuthor(List<String> lines, Map<String, String> parameters) {
+        return null;
+    }
+
+    private List<String> filterFrom(List<String> lines, Map<String, String> parameters) {
+        return null;
+    }
+
+    private List<String> filterTo(List<String> lines, String parameter) {
+        if (parameter == null) return lines;
+        return null;
+    }
+
+
+    private static List<String> filterLines(List<String> lines, Map<String, String> parameters) {
+        return null;
+    }
+
+
+    private static Metadata getMetadata(String uuid) {
+        if (!metadatas.containsKey(uuid)) metadatas.put(uuid, deserializer.deserialize(getMetadataJson(uuid)));
+        return metadatas.get(uuid);
+    }
+
+    private static String getMetadataJson(String uuid) {
+        try {
+            return Files.readString(Path.of(System.getenv("DATAMART") + "/metadata/content/" + uuid));
+        } catch (IOException e) {
+            throw new RuntimeException(e);
+        }
     }
 
     private static List<String> getLines(Path path) {
         try {
             if (!Files.exists(path))
                 return List.of(path.getFileName().toString() + " does not exist");
-            return Files.readAllLines(path);
+            List<String> lines = Files.readAllLines(path);
+            lines.remove(0);
+            return lines;
         } catch (IOException e) {
             throw new RuntimeException(e);
         }
