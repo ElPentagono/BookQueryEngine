167289748013018f387b6b22d45cf9fb73d715bf
Jose Juan <josejuanhernandezgalvez@gmail.com>
2022-12-02 21:26:57 +0000

    Api command semi-done

--- webservice/src/main/java/es/pentagono/WebService.java
+++ webservice/src/main/java/es/pentagono/WebService.java
@@ -1,4 +1,30 @@
 package es.pentagono;
 
+import spark.Request;
+import spark.Spark;
+
+import java.util.HashMap;
+import java.util.Map;
+
 public class WebService {
+    private final Map<String, Command> commands;
+
+    public WebService() {
+        this.commands = new HashMap<>();
+    }
+
+    public void add(String route, Command command) {
+        commands.put(route, command);
+    }
+
+    public void start() {
+        Spark.port(8080);
+        for (String route : commands.keySet()) {
+            Spark.get(route, (req, res) -> commands.get(route).execute(parametersIn(req)));
+        }
+    }
+
+    private Map<String, String> parametersIn(Request request) {
+        return request.params();
+    }
 }
