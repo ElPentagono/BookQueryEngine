76871fdc7daa6a6a94b143f07118a73b75a4f97f
Davoestacogido <90717947+Davoestacogido@users.noreply.github.com>
2022-12-12 18:16:55 +0000

    naming changed

--- crawler/src/main/java/es/pentagono/parsers/GutenbergDownloadEventParser.java
+++ crawler/src/main/java/es/pentagono/parsers/GutenbergDownloadEventParser.java
@@ -21,43 +21,43 @@ public class GutenbergDownloadEventParser implements EventParser {
 
     private Map<String, String> getMetadataFromBook(String book) {
         Map<String, String> metadata = new HashMap<>();
-        addFieldsToMetadata(getPreContent(book), metadata);
+        addFieldsToMetadata(getMetadataSection(book), metadata);
         return metadata;
     }
 
-    private String getPreContent(String book) {
+    private String getMetadataSection(String book) {
         Matcher matcherStart = Pattern.compile("\\*\\*\\* START OF (THIS|THE) PROJECT GUTENBERG EBOOK .*?\\*\\*\\*", Pattern.DOTALL).matcher(book);
         return (matcherStart.find()) ? book.substring(0, matcherStart.start()) : book;
     }
 
-    private void addFieldsToMetadata(String preContent, Map<String, String> metadata) {
-        addTitle(preContent, metadata);
-        addAuthor(preContent, metadata);
-        addLanguage(preContent, metadata);
-        addReleaseDate(preContent, metadata);
+    private void addFieldsToMetadata(String metadataSection, Map<String, String> metadata) {
+        addTitle(metadataSection, metadata);
+        addAuthor(metadataSection, metadata);
+        addLanguage(metadataSection, metadata);
+        addReleaseDate(metadataSection, metadata);
     }
 
-    private void addReleaseDate(String preContent, Map<String, String> metadata) {
-        metadata.put("releaseDate",  getMetadataFieldFromPattern(preContent, Pattern.compile("Release Date(.+[\r\n])+"))
+    private void addReleaseDate(String metadataSection, Map<String, String> metadata) {
+        metadata.put("releaseDate",  getMetadataFieldFromPattern(metadataSection, Pattern.compile("Release Date(.+[\r\n])+"))
                 .replaceAll("\\[.*]","").trim());
     }
 
-    private void addLanguage(String preContent, Map<String, String> metadata) {
-        metadata.put("language",  getMetadataFieldFromPattern(preContent, Pattern.compile("Language(.+[\r\n])+")));
+    private void addLanguage(String metadataSection, Map<String, String> metadata) {
+        metadata.put("language",  getMetadataFieldFromPattern(metadataSection, Pattern.compile("Language(.+[\r\n])+")));
     }
 
-    private void addAuthor(String preContent, Map<String, String> metadata) {
-        metadata.put("author",  getMetadataFieldFromPattern(preContent, Pattern.compile("Author(.+[\r\n])+")));
+    private void addAuthor(String metadataSection, Map<String, String> metadata) {
+        metadata.put("author",  getMetadataFieldFromPattern(metadataSection, Pattern.compile("Author(.+[\r\n])+")));
     }
 
-    private void addTitle(String preContent, Map<String, String> metadata) {
-        metadata.put("title",  getMetadataFieldFromPattern(preContent, Pattern.compile("Title(.+[\r\n])+")));
+    private void addTitle(String metadataSection, Map<String, String> metadata) {
+        metadata.put("title",  getMetadataFieldFromPattern(metadataSection, Pattern.compile("Title(.+[\r\n])+")));
     }
 
-    private String getMetadataFieldFromPattern(String preContent, Pattern pattern) {
-        Matcher matcher = pattern.matcher(preContent);
+    private String getMetadataFieldFromPattern(String metadataSection, Pattern pattern) {
+        Matcher matcher = pattern.matcher(metadataSection);
         return ((matcher.find()) ?
-            preContent.substring(matcher.start(), matcher.end() - 1).split(":", 2)[1].trim() : "");
+            metadataSection.substring(matcher.start(), matcher.end() - 1).split(":", 2)[1].trim() : "");
     }
 
     private String content(String book) {
