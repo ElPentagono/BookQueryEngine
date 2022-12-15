a4bb00d882674ec5fc7286c05ce6498ad236ab7a
Davoestacogido <davidcruzsanchez111@gmail.com>
2022-12-15 20:17:22 +0000

    starting finding null values

--- metadata/src/main/java/es/pentagono/serializers/SQLMetadataSerializer.java
+++ metadata/src/main/java/es/pentagono/serializers/SQLMetadataSerializer.java
@@ -3,12 +3,24 @@ package es.pentagono.serializers;
 import es.pentagono.Metadata;
 import es.pentagono.MetadataSerializer;
 
+import java.util.Objects;
+
 public class SQLMetadataSerializer implements MetadataSerializer {
     @Override
     public String serialize(Metadata metadata) {
-        return "'" + metadata.title().replaceAll("'","") + "','"
-                + metadata.author().replaceAll("'","") + "','"
-                + metadata.language().replaceAll("'","") + "','"
+        findNullValues(
+                metadata.title().replaceAll("'",""),
+                metadata.author().replaceAll("'",""),
+                metadata.language().replaceAll("'",""),
+                metadata
+                );
+        return "'" + metadata.title() + "','"
+                + metadata.author() + "','"
+                + metadata.language() + "','"
                 + metadata.releaseDate() + "'";
+    }
+
+    private void findNullValues(String title, String author, String language, Metadata metadata) {
+
     }
 }
